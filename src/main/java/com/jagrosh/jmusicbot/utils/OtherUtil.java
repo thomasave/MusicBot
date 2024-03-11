62e2787f0b165759d99c18f8eb018a0bebd6c207
Thomas Avé <email@thomasave.be>
2024-03-11 15:13:23 +0100

    Disable unsupported bot checking

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -45,12 +45,12 @@ public class OtherUtil
                     + "New Version: %s\n\n"
                     + "Please visit https://github.com/jagrosh/MusicBot/releases/latest to get the latest release.";
     private final static String WINDOWS_INVALID_PATH = "c:\\windows\\system32\\";
-    
+
     /**
      * gets a Path from a String
      * also fixes the windows tendency to try to start in system32
      * any time the bot tries to access this path, it will instead start in the location of the jar file
-     * 
+     *
      * @param path the string path
      * @return the Path object
      */
@@ -68,10 +68,10 @@ public class OtherUtil
         }
         return result;
     }
-    
+
     /**
      * Loads a resource from the jar as a string
-     * 
+     *
      * @param clazz class base object
      * @param name name of resource
      * @return string containing the contents of the resource
@@ -89,10 +89,10 @@ public class OtherUtil
             return null;
         }
     }
-    
+
     /**
      * Loads image data from a URL
-     * 
+     *
      * @param url url of image
      * @return inputstream of url
      */
@@ -100,7 +100,7 @@ public class OtherUtil
     {
         if(url==null)
             return null;
-        try 
+        try
         {
             URL u = new URL(url);
             URLConnection urlConnection = u.openConnection();
@@ -110,10 +110,10 @@ public class OtherUtil
         catch(IOException | IllegalArgumentException ignore) {}
         return null;
     }
-    
+
     /**
      * Parses an activity from a string
-     * 
+     *
      * @param game the game, including the action such as 'playing' or 'watching'
      * @return the parsed activity
      */
@@ -140,12 +140,12 @@ public class OtherUtil
         }
         return Activity.playing(game);
     }
-   
+
     public static String makeNonEmpty(String str)
     {
         return str == null || str.isEmpty() ? "\u200B" : str;
     }
-    
+
     public static OnlineStatus parseStatus(String status)
     {
         if(status==null || status.trim().isEmpty())
@@ -153,28 +153,28 @@ public class OtherUtil
         OnlineStatus st = OnlineStatus.fromKey(status);
         return st == null ? OnlineStatus.ONLINE : st;
     }
-    
+
     public static void checkJavaVersion(Prompt prompt)
     {
         if(!System.getProperty("java.vm.name").contains("64"))
-            prompt.alert(Prompt.Level.WARNING, "Java Version", 
+            prompt.alert(Prompt.Level.WARNING, "Java Version",
                     "It appears that you may not be using a supported Java version. Please use 64-bit java.");
     }
-    
+
     public static void checkVersion(Prompt prompt)
     {
         // Get current version number
         String version = getCurrentVersion();
-        
+
         // Check for new version
         String latestVersion = getLatestVersion();
-        
+
         if(latestVersion!=null && !latestVersion.equals(version))
         {
             prompt.alert(Prompt.Level.WARNING, "JMusicBot Version", String.format(NEW_VERSION_AVAILABLE, version, latestVersion));
         }
     }
-    
+
     public static String getCurrentVersion()
     {
         if(JMusicBot.class.getPackage()!=null && JMusicBot.class.getPackage().getImplementationVersion()!=null)
@@ -182,7 +182,7 @@ public class OtherUtil
         else
             return "UNKNOWN";
     }
-    
+
     public static String getLatestVersion()
     {
         try
@@ -216,14 +216,14 @@ public class OtherUtil
      * Checks if the bot JMusicBot is being run on is supported & returns the reason if it is not.
      * @return A string with the reason, or null if it is supported.
      */
-    public static String getUnsupportedBotReason(JDA jda) 
+    public static String getUnsupportedBotReason(JDA jda)
     {
-        if (jda.getSelfUser().getFlags().contains(User.UserFlag.VERIFIED_BOT))
-            return "The bot is verified. Using JMusicBot in a verified bot is not supported.";
-
-        ApplicationInfo info = jda.retrieveApplicationInfo().complete();
-        if (info.isBotPublic())
-            return "\"Public Bot\" is enabled. Using JMusicBot as a public bot is not supported. Please disable it in the Developer Dashboard.";
+        // if (jda.getSelfUser().getFlags().contains(User.UserFlag.VERIFIED_BOT))
+        //     return "The bot is verified. Using JMusicBot in a verified bot is not supported.";
+        //
+        // ApplicationInfo info = jda.retrieveApplicationInfo().complete();
+        // if (info.isBotPublic())
+        //     return "\"Public Bot\" is enabled. Using JMusicBot as a public bot is not supported. Please disable it in the Developer Dashboard.";
 
         return null;
     }
